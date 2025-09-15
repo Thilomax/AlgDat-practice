b7e5b624911375e7b56ded2e4f83679547135dbb
Thilomax <thove6344@oslomet.no>
2025-09-15 13:26:37 +0200

    uhhh begynte på lister kode i forelesning?

---
+++ src/ForelesningerØving/F8_Lister/Dato_09_15_Lister.java
@@ -0,0 +1,10 @@
+package ForelesningerØving.F8_Lister;
+
+public class Dato_09_15_Lister {
+    public static void main(String[] args) {
+
+    }
+    class TabellListe<T> implements Liste<T>{
+
+    }
+}
